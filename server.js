c4687cd5f6cdf0c68008efc5d01e16cbbb604049
Cecil Lee <cmjlee3@gmail.com>
2016-11-07 16:15:05 -0500

    wtf

--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const logger  = require('morgan');
+const homeRoute = require('./routes/index');
+const taskRoute = require('./routes/tasks');
 
 // this tests to see if we have NODE_ENV in our environment
 // Only load the dotenv if we need it
@@ -21,7 +23,8 @@ app.use(bodyParser.json());
 
 // bring in the task routes
 // app.use('/tasks', require('./routes/tasks'));
-// app.use('/', require('./routes/index'));
+app.use('/', homeRoute);
+app.use('/', taskRoute);
 
 app.use((err, req, res, next) => {
   console.error(err);
